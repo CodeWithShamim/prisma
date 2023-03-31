8e84f4171af2a29cf0398e00bfa6778b72820e45
codewithshamim <shamimislamonline@gmail.com>
2023-03-31 23:21:10 +0600

    updated

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # prisma
+
+### basic command
+- npm install prisma typescript ts-node @types/node --save-dev
+- npm install @prisma/client
+- npx tsc --init
+- npx prisma init --datasource-provider postgresql(db name)
+- npx prisma migrate dev --name init
+- npx prisma generate
+- npx prisma studio
--- script.ts
+++ script.ts
@@ -2,14 +2,33 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const main = async () => {
-  //   const result = await prisma.user.create({
-  //     data: {
-  //       email: "shamim@gmail.com",
-  //       name: "shamim",
+  // const result = await prisma.user.create({
+  //   data: {
+  //     email: "shamim3@gmail.com",
+  //     name: "shamim3",
+  //   },
+  // });
+
+  // const result = await prisma.user.create({
+  //   data: {
+  //     email: "shamim4@gmail.com",
+  //     name: "shamim4",
+  //     posts: {
+  //       create: {
+  //         title: "test title",
+  //       },
   //     },
-  //   });
-  const result = await prisma.user.findMany();
-  console.log(result);
+  //   },
+  // });
+  const result = await prisma.user.findMany({
+    where: {
+      id: 8,
+    },
+    include: {
+      posts: true,
+    },
+  });
+  console.dir(result, { depth: null })
 };
 
 main()
